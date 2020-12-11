bdcfc88ba223acb6a815a0b4637655f3ecdffec3
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2020-12-11 15:24:42 -0800

    added password reset/change templates

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from users.views import dashboard
 
 urlpatterns = [
-    url(r"^dashboard/", dashboard, name="dashboard"),
     url(r"^accounts/", include("django.contrib.auth.urls")),
+    url(r"^dashboard/", dashboard, name="dashboard"),
 ]
